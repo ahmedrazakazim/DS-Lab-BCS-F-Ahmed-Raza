f9071ce3c5954b2045f152d1ecbad49ce0794a06
Ahmed Raza <2004ahmadraza@gmail.com>
2025-08-24 15:11:53 +0500

    Update Q1.cpp

--- Lab 01/Q1.cpp
+++ Lab 01/Q1.cpp
@@ -1,1 +1,59 @@
+#include <iostream>
 
+using namespace std;
+
+class BankAccount
+{
+private:
+    static int count;
+    int ID;
+    int balance;
+
+public:
+    // default
+    BankAccount()
+    {
+        balance = 0;
+        count++;
+        ID = count;
+    }
+    // parameterized
+    BankAccount(int amount)
+    {
+        balance = amount;
+        count++;
+        ID = count;
+    }
+    // copy
+    BankAccount(const BankAccount &obj)
+    {
+        this->balance = obj.balance;
+        count++;
+        ID = count;
+    }
+    void displayBalance()
+    {
+        cout << "The balance of account " << this->ID << " is: $" << balance << endl;
+    }
+    void deductBalance(int deduction)
+    {
+        this->balance = balance - deduction;
+    }
+};
+int BankAccount::count = 0;
+
+int main()
+{
+    BankAccount account1;
+    account1.displayBalance();
+
+    BankAccount account2(1000);
+    account2.displayBalance();
+
+    BankAccount account3(account2);
+    account3.displayBalance();
+    account3.deductBalance(200);
+    account3.displayBalance();
+    account2.displayBalance();
+    return 0;
+}
