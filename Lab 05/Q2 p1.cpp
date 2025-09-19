1e8397a61865ef5d5793e58adb0f8c14a5be2837
Ahmed Raza <2004ahmadraza@gmail.com>
2025-09-19 12:11:10 +0500

    Create Q2 p1.cpp

---
+++ Lab 05/Q2 p1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void printNumbers(int n) 
+{
+    if (n >= 1) {
+        cout << n << endl;
+        printNumbers(n - 1);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+cin >> n;
+    printNumbers(n); 
+
+  return 0;
+}
