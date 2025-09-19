68f7e192e740e931096a3cd304904372b6ab46ef
Ahmed Raza <2004ahmadraza@gmail.com>
2025-09-19 12:08:01 +0500

    Update Q1.cpp

--- Lab 05/Q1.cpp
+++ Lab 05/Q1.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std:
+
+int calculateFactorial(int n) {
+if (n == 1) {
+  return 1;
+}
+else
+  return n*calculateFactorial(n-1); 
+
+}
+
+int main() {
+int n;
+  cout << "Enter a number: \n";
+cin >> n;
+  calculateFactorial(n);
+
+  return 0;
+}
